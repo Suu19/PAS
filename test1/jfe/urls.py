aaa7eedf9436211fc9ab6fc5e7cac1380bc2b63e
Suu <nawthae@jss1.jp>
2022-06-21 18:22:40 +0900

    test delete

--- test1/jfe/urls.py
+++
@@ -1,9 +0,0 @@
-from jfe import views
-from django.urls import path
-from django.contrib import admin
-
-urlpatterns=[
-    path("homepage/<int:id>",views.HomePageView,name='home'),
-    path('', views.aboutPageView,name=''),
-    path('list',views.listPageView,name='list')
-]
